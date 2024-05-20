0758c3d0953b8aa1df7b8a04f4b1caf3c8cbf21a
yiyaaa <thdtjs1219@gmail.com>
2024-05-20 23:19:32 +0900

    feat: apply function interface

--- src/main/java/baseball/exception/BaseballException.java
+++ src/main/java/baseball/exception/BaseballException.java
@@ -1,54 +1,75 @@
 package baseball.exception;
 
+import baseball.global.message.ExceptionMessage;
+
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.function.Consumer;
 
 public class BaseballException {
 
     private final int NUMBERLENGTH = 3;
 
+    private void validateInput(
+            final String number,
+            final Consumer<String> validator
+    ) {
+        validator.accept(number);
+    }
+
     public void checkNumberSize(
             final String number
     ) {
-        if (number.length() < NUMBERLENGTH || number.length() > NUMBERLENGTH) {
-            throw new IllegalArgumentException("숫자의 길이가 잘못되었습니다");
-        }
+        validateInput(number, num -> {
+            if (num.length() < NUMBERLENGTH || num.length() > NUMBERLENGTH) {
+                throw new IllegalArgumentException(ExceptionMessage.NUMBERSIZE.getMessage());
+            }
+        });
     }
 
     public void checkOverlap(
             final String number
     ) {
-        String[] numbers = number.split("");
-        Set<String> setList = new HashSet<>(Arrays.asList(numbers));
-        if (setList.size() < NUMBERLENGTH) {
-            throw new IllegalArgumentException("중복된 숫자가 존재합니다");
-        }
+        validateInput(number, num -> {
+            String[] numbers = num.split("");
+            Set<String> setList = new HashSet<>(Arrays.asList(numbers));
+            if (setList.size() < NUMBERLENGTH) {
+                throw new IllegalArgumentException(ExceptionMessage.OVERLAP.getMessage());
+            }
+        });
     }
 
     public void checkNothing(
             final String number
     ) {
-        if (number.isEmpty()) {
-            throw new IllegalArgumentException("입력을 하고 enter를 쳐주세요");
-        }
+        validateInput(number, num -> {
+            if (num.isEmpty()) {
+                throw new IllegalArgumentException(ExceptionMessage.NOTHING.getMessage());
+            }
+        });
     }
 
     public void checkEtc(
             final String number
     ) {
-        String regex = "[0-9]+";
-        if (!number.matches(regex)) {
-            throw new IllegalArgumentException("숫자를 입력해주세요");
-        }
+        validateInput(number, num -> {
+            String regex = "[1-9]+";
+            if (!num.matches(regex)) {
+                throw new IllegalArgumentException(ExceptionMessage.ETC.getMessage());
+            }
+        });
     }
 
     public void checkRestartKey(
             final String number
     ) {
-        String regex = "[12]";
-        if (!number.matches(regex)) {
-            throw new IllegalArgumentException("1과 2만 입력이 가능합니다");
-        }
+        validateInput(number, num -> {
+            String regex = "[12]";
+            if (!num.matches(regex)) {
+                throw new IllegalArgumentException(ExceptionMessage.RESTARTKEY.getMessage());
+            }
+        });
     }
+
 }
