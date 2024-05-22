ce6708a6cb60374a9d1cead77088195a393ee3b1
oyeonjoo <oyj0842@naver.com>
2024-05-22 22:32:59 +0900

    :recycle: INPUT_MSG -> INPUT_MSG.getMessage() 로 수정

--- src/main/java/baseball/presentation/Scanner.java
+++ src/main/java/baseball/presentation/Scanner.java
@@ -1,4 +1,4 @@
-package baseball.io;
+package baseball.presentation;
 
 import static baseball.constant.Message.INPUT_MSG;
 
@@ -7,7 +7,7 @@ import camp.nextstep.edu.missionutils.Console;
 public class Scanner {
 
     public String getPlayerNumbers() {
-        System.out.print(INPUT_MSG);
+        System.out.print(INPUT_MSG.getMessage());
 
         return Console.readLine();
     }
