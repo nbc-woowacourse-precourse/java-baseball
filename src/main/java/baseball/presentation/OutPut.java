416bca5ada15311138e71c2339048492226f0473
oyeonjoo <oyj0842@naver.com>
2024-05-22 23:42:05 +0900

    :recycle: 불필요한 메세지 및 메소드 삭제

--- src/main/java/baseball/presentation/OutPut.java
+++ src/main/java/baseball/presentation/OutPut.java
@@ -1,13 +1,9 @@
 package baseball.presentation;
 
-import static baseball.constant.MenuChoice.RESTART;
-import static baseball.constant.MenuChoice.TERMINATE;
-import static baseball.constant.Message.ERROR_MSG;
+import static baseball.constant.Message.INPUT_MSG;
 import static baseball.constant.Message.RESTART_MSG;
 import static baseball.constant.Message.START_MSG;
 import static baseball.constant.Message.SUCCESS_MSG;
-
-import camp.nextstep.edu.missionutils.Console;
 
 public class OutPut {
 
@@ -15,24 +11,12 @@ public class OutPut {
         System.out.println(START_MSG.getMessage());
     }
 
+    public void inputMsg() {
+        System.out.print(INPUT_MSG.getMessage());
+    }
+
     public void wonTheGame() {
         System.out.println(SUCCESS_MSG.getMessage());
         System.out.println(RESTART_MSG.getMessage());
-    }
-
-    public void restartGame() {
-        String restart = Console.readLine();
-
-        if (restart.equals(RESTART.getMenuChoice())) {
-            System.out.println(START_MSG.getMessage());
-        }
-    }
-
-    public void terminateGame() {
-        String restart = Console.readLine();
-
-        if (!restart.equals(TERMINATE.getMenuChoice())) {
-            throw new IllegalArgumentException(ERROR_MSG.getMessage());
-        }
     }
 }
