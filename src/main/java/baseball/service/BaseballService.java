327556630dedc5f6a7fc0a0fcccfef1cb8432dd5
oyeonjoo <oyj0842@naver.com>
2024-05-22 23:46:04 +0900

    :recycle: 레이어 분리에 따른 메소드 분리, 검증 메소드 수정, 함수형 인터페이스 추가에 따른 수정

--- src/main/java/baseball/service/BaseballService.java
+++ src/main/java/baseball/service/BaseballService.java
@@ -1,5 +1,9 @@
 package baseball.service;
 
+import static baseball.constant.Hint.BALL;
+import static baseball.constant.Hint.NOTHING;
+import static baseball.constant.Hint.STRIKE;
+import static baseball.constant.Message.ERROR_MSG;
 import static baseball.constant.Number.DIGITS;
 
 import java.util.ArrayList;
@@ -7,12 +11,35 @@ import java.util.List;
 
 public class BaseballService {
 
-    // Todo: 검증 메소드 구현
-    public boolean validateInput(String input) {
-        return input.matches("[1-9]{3}")
-            && input.chars().distinct().count() == DIGITS.getNumber();
+    public void getInput(String input) {
+        if (!validateInput(input)) {
+            throw new IllegalArgumentException(ERROR_MSG.getMessage());
+        }
     }
-    // Todo : String 을 Integer 로 바꿔야 하는지 다시 생각해볼 것
+
+    public boolean validateInput(String input) {
+        return validateInputDigits(input) && validateInputDuplicated(input);
+    }
+
+    public boolean validateInputDigits(String input) {
+        return input.matches("\\d{" + DIGITS.getNumber() + "}");
+    }
+
+    public boolean validateInputDuplicated(String input) {
+        return input.chars().distinct().count() == DIGITS.getNumber();
+    }
+
+    public int getStrike(String input, List<Integer> computerNumbers) {
+        List<Integer> playerNumbers = convertInputToNumber(input);
+        int strike = getCount(computerNumbers, playerNumbers,
+            strikeCountCalculator);
+        int ball = getCount(computerNumbers, playerNumbers,
+            ballCountCalculator);
+
+        System.out.println(makeHint(strike, ball));
+        return strike;
+    }
+
     public List<Integer> convertInputToNumber(String input) {
         List<Integer> numbers = new ArrayList<>();
 
@@ -23,4 +50,51 @@ public class BaseballService {
         }
         return numbers;
     }
+
+    public String makeHint(int strike, int ball) {
+        StringBuilder sb = new StringBuilder();
+        if (strike != 0) {
+            if (ball != 0) {
+                sb.append(ball).append(BALL.getHint())
+                    .append(strike).append(STRIKE.getHint());
+                return sb.toString();
+            }
+            sb.append(strike).append(STRIKE.getHint());
+            return sb.toString();
+        }
+        if (ball != 0) {
+            sb.append(ball).append(BALL.getHint());
+            return sb.toString();
+        }
+        sb.append(NOTHING.getHint());
+        return sb.toString();
+    }
+
+    public int getCount(List<Integer> computerNumbers, List<Integer> playerNumbers,
+        CountCalculator calculator) {
+        return calculator.calculate(computerNumbers, playerNumbers);
+    }
+
+    public CountCalculator ballCountCalculator = (computerNumbers, playerNumbers) -> {
+        int ballCount = 0;
+
+        for (int i = 0; i < DIGITS.getNumber(); i++) {
+            if (!computerNumbers.get(i).equals(playerNumbers.get(i))
+                && playerNumbers.contains(computerNumbers.get(i))) {
+                ballCount++;
+            }
+        }
+        return ballCount;
+    };
+
+    public CountCalculator strikeCountCalculator = (computerNumbers, playerNumbers) -> {
+        int strikeCount = 0;
+
+        for (int i = 0; i < DIGITS.getNumber(); i++) {
+            if (computerNumbers.get(i).equals(playerNumbers.get(i))) {
+                strikeCount++;
+            }
+        }
+        return strikeCount;
+    };
 }
