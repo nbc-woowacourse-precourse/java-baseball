6283fdaa6d117ff02a7521ffe555da76e9349338
oyeonjoo <oyj0842@naver.com>
2024-05-22 22:32:05 +0900

    :recycle: 상수 추가로 인한 코드 변경

--- src/main/java/baseball/constant/Number.java
+++ src/main/java/baseball/constant/Number.java
@@ -2,6 +2,7 @@ package baseball.constant;
 
 public enum Number {
 
+    START_NUM(1), LAST_NUM(9),
     DIGITS(3), THREE_STRIKE(3);
 
     private final int number;
--- src/main/java/baseball/model/Computer.java
+++ src/main/java/baseball/model/Computer.java
@@ -1,6 +1,8 @@
 package baseball.model;
 
 import static baseball.constant.Number.DIGITS;
+import static baseball.constant.Number.LAST_NUM;
+import static baseball.constant.Number.START_NUM;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
@@ -12,7 +14,7 @@ public class Computer {
         List<Integer> numbers = new ArrayList<>();
 
         while (numbers.size() < DIGITS.getNumber()) {
-            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            int randomNumber = Randoms.pickNumberInRange(START_NUM.getNumber(), LAST_NUM.getNumber());
 
             if (!numbers.contains(randomNumber)) {
                 numbers.add(randomNumber);
