acb43ca60deeb963288d960a6b678f87e386432b
oyeonjoo <oyj0842@naver.com>
2024-05-22 22:36:16 +0900

    :recycle: 클래스명 변경

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,12 +1,12 @@
 package baseball;
 
-import baseball.model.Player;
+import baseball.model.BaseballGame;
 
 public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        Player player = new Player();
-        player.startGame();
+        BaseballGame baseballGame = new BaseballGame();
+        baseballGame.start();
     }
 }
