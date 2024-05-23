d999aff473c1a834e412b829451af1e6802f8591
yiyaaa <thdtjs1219@gmail.com>
2024-05-23 17:17:26 +0900

    feat: add function interface

--- src/main/java/baseball/exception/BaseballException.java
+++ src/main/java/baseball/exception/BaseballException.java
@@ -4,8 +4,7 @@ import baseball.global.message.ExceptionMessage;
 
 import java.util.Arrays;
 import java.util.HashSet;
-import java.util.Set;
-import java.util.function.Consumer;
+import java.util.function.Function;
 
 public class BaseballException {
 
@@ -14,63 +13,54 @@ public class BaseballException {
     private final String ETC = "[1-9]+";
     private final String RESTARTKEY = "[12]";
 
-    private void validateInput(
+    public void validateInput(
             final String number,
-            final Consumer<String> validator
+            final Function<String, Boolean> validator,
+            final String errorMessage
     ) {
-        validator.accept(number);
+        if (!validator.apply(number)) {
+            throw new IllegalArgumentException(errorMessage);
+        }
     }
 
-    public void checkNumberSize(
-            final String number
-    ) {
-        validateInput(number, num -> {
-            if (num.length() < NUMBERLENGTH || num.length() > NUMBERLENGTH) {
-                throw new IllegalArgumentException(ExceptionMessage.NUMBERSIZE.getMessage());
-            }
-        });
+    public void checkNumberSize(final String number) {
+        validateInput(
+                number,
+                num -> num.length() == NUMBERLENGTH,
+                ExceptionMessage.NUMBERSIZE.getMessage()
+        );
     }
 
-    public void checkOverlap(
-            final String number
-    ) {
-        validateInput(number, num -> {
-            String[] numbers = num.split("");
-            Set<String> setList = new HashSet<>(Arrays.asList(numbers));
-            if (setList.size() < NUMBERLENGTH) {
-                throw new IllegalArgumentException(ExceptionMessage.OVERLAP.getMessage());
-            }
-        });
+    public void checkOverlap(final String number) {
+        validateInput(
+                number,
+                num -> new HashSet<>(Arrays.asList(num.split(""))).size() == NUMBERLENGTH,
+                ExceptionMessage.OVERLAP.getMessage()
+        );
     }
 
-    public void checkNothing(
-            final String number
-    ) {
-        validateInput(number, num -> {
-            if (num.isEmpty()) {
-                throw new IllegalArgumentException(ExceptionMessage.NOTHING.getMessage());
-            }
-        });
+    public void checkNothing(final String number) {
+        validateInput(
+                number,
+                num -> !num.isEmpty(),
+                ExceptionMessage.NOTHING.getMessage()
+        );
     }
 
-    public void checkEtc(
-            final String number
-    ) {
-        validateInput(number, num -> {
-            if (!num.matches(ETC)) {
-                throw new IllegalArgumentException(ExceptionMessage.ETC.getMessage());
-            }
-        });
+    public void checkEtc(final String number) {
+        validateInput(
+                number,
+                num -> num.matches(ETC),
+                ExceptionMessage.ETC.getMessage()
+        );
     }
 
-    public void checkRestartKey(
-            final String number
-    ) {
-        validateInput(number, num -> {
-            if (!num.matches(RESTARTKEY)) {
-                throw new IllegalArgumentException(ExceptionMessage.RESTARTKEY.getMessage());
-            }
-        });
+    public void checkRestartKey(final String number) {
+        validateInput(
+                number,
+                num -> num.matches(RESTARTKEY),
+                ExceptionMessage.RESTARTKEY.getMessage()
+        );
     }
 
 }
